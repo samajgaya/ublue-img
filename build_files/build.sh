8c887fc4f9d79b1332bfb5a021bdb5c4a8a6cf1d
Aniruddh Ganesh <aniruddhganesh@protonmail.com>
2026-04-17 19:31:51 +0530

    truly custom!

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,17 @@
 
 set -ouex pipefail
 
-### Install packages
+dnf install -y  \
+	network-manager \
+	iwd \
+	xorg-x11-server-Xorg \
+	xorg-x11-drv-libinput \
+	mesa-dri-drivers \
+	google-noto-sans-fonts \
+	google-noto-serif-fonts \
+	wayland-utils \
+	libXft
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+systemctl enable NetworkManager
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+dnf clean all
